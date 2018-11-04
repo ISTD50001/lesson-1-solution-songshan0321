f63f7398d14c4648e5ba65a14c518a781620d85a
SongShanYou <songshan_you@mymail.sutd.edu.sg>
2018-11-04 15:53:55 +0800

    Completed 9 TODOs

--- app/src/main/java/com/example/norman_lee/lesson1cohort3/MainActivity.java
+++ app/src/main/java/com/example/norman_lee/lesson1cohort3/MainActivity.java
@@ -7,47 +7,72 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-
-//TODO 1.1 Put in the images in the drawables folder
-//TODO 1.2 Go to activity_main.xml and put in the layout
+//1.1 Put in the images in the drawables folder
+//1.2 Go to activity_main.xml and put in the layout
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Integer> images;
     Button charaButton;
     ImageView charaImage;
-    int count = 0;
+    Button resetButton;
+    Random r;
+    int count = 0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO 1.3 Instantiate an ArrayList object
+        //1.3 Instantiate an ArrayList object
         images = new ArrayList<>();
+
+        //1.4 Add the image IDs to the ArrayList
+        images.add(R.drawable.ashketchum);
         images.add(R.drawable.bartsimpson);
+        images.add(R.drawable.doraemon);
         images.add(R.drawable.dory);
         images.add(R.drawable.edogawaconan);
-        //TODO 1.4 Add the image IDs to the ArrayList
-        //TODO 1.5 Get references to charaButton and charaImage using findViewById
+        images.add(R.drawable.garfield);
+        images.add(R.drawable.judyhopps);
+        images.add(R.drawable.nickwilde);
+        images.add(R.drawable.pikachu);
+        images.add(R.drawable.tomandjerry);
+        images.add(R.drawable.totoro);
+        images.add(R.drawable.yoda);
+        images.add(R.drawable.wolverine);
+
+        //Get references to charaButton and charaImage using findViewById
         charaButton = findViewById(R.id.charaButton);
         charaImage = findViewById(R.id.charaImage);
-        //TODO 1.6 For charaButton, invoke the setOnClickListenerMethod
-        //TODO 1.7 Create an anonymous class which implements View.OnClickListener
-        //TODO 1.8 Write code to randomly select an image ID from the ArrayList and display it in the ImageView
+        resetButton = findViewById(R.id.resetButton);
+
+        //For charaButton, invoke the setOnClickListenerMethod
+        //Create an anonymous class which implements View.OnClickListener
+        //Write code to randomly select an image ID from the ArrayList and display it in the ImageView
         charaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                int index = count % images.size();
-                count = count + 1;
+                count ++;
+                r = new Random();
+                int index = r.nextInt(images.size());
                 int id = images.get(index);
                 charaImage.setImageResource(id);
-
             }
         });
-        //TODO 1.9 [On your own] Create another Button to always display the first image
+
+        resetButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                int index = 0;
+                int id = images.get(index);
+                charaImage.setImageResource(id);
+            }
+        });
 
     }
 }
